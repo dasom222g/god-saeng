0ded5843ba7ec3558d54ca0d92fbde3e9616973b
dasom <50973005+dasom222g@users.noreply.github.com>
2022-06-20 23:01:19 +0900

    Fix router

--- src/routes/question.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-
-const Question: FC = () => {
-  // view
-  return <div className="-mb-4 md:-mb-6">question</div>
-}
-
-export default Question
---
+++ src/routes/step.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+
+const Step: FC = () => {
+  // view
+  return <div className="-mb-4 md:-mb-6">Step</div>
+}
+
+export default Step
