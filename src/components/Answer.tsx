d2a924c96e2252d90328aed0ae7532044e285adb
dasom <dasom228@gmail.com>
2022-06-27 00:03:13 +0900

    Add navigate

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,18 +1,25 @@
 import React, { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { MBTIAnswerType } from '../lib/type'
 
 interface AnswerProp {
   answer: MBTIAnswerType
 }
-// `/step?page=${answer.questionNextCode}`
 
 const Answer: FC<AnswerProp> = ({ answer }) => {
+  const navigate = useNavigate()
   const { answerList } = answer
+
+  const handleSelect = (): void => {
+    navigate(`/step?page=${answer.questionNextCode}`)
+    // navigate('/result')
+  }
+
   return (
     <div>
       {answerList.map((item) => (
         <div className="py-2" key={item.code}>
-          <button type="button">
+          <button type="button" className="block w-full" onClick={handleSelect}>
             <span className="block px-4 py-4 bg-white rounded-xl">{item.text}</span>
           </button>
         </div>
