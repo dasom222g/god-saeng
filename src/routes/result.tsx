b885e39ac4326b31a267aa0d28697810c025438c
dasom <50973005+dasom222g@users.noreply.github.com>
2022-07-27 15:35:54 +0900

    Add resultMbti logic

--- src/routes/result.tsx
+++ src/routes/result.tsx
@@ -1,15 +1,24 @@
-import React, { FC } from 'react'
-import { useRecoilValue } from 'recoil'
-import { resultState } from '../state/dataState'
+import React, { FC, useEffect } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { resultMbtiState, resultState } from '../state/dataState'
 
 const Result: FC = () => {
   const result = useRecoilValue(resultState)
+  const [resultMbti, setResultMbti] = useRecoilState(resultMbtiState)
+
+  useEffect(() => {
+    const sortData = [...result].sort((a, b) => a.qustionCode - b.qustionCode)
+    const mbti = sortData.reduce(
+      (acc, current) =>
+        (current.resultValue[current.firstType] as number) > 1
+          ? acc + current.firstType
+          : acc + current.lastType,
+      '',
+    )
+    setResultMbti(mbti)
+  }, [result, setResultMbti])
   // view
-  return (
-    <div className="-mb-4 md:-mb-6">
-      Result: {result}
-    </div>
-  )
+  return <div className="-mb-4 md:-mb-6">Result: {resultMbti}</div>
 }
 
 export default Result
