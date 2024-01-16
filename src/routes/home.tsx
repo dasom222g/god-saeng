6fed4994fa43c01eb9b0f079a852a2b928e7c8db
dasom222g <dasom228@gmail.com>
2024-01-16 23:48:45 +0900

    Fix link tag

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,14 +7,18 @@ const Home: FC = () => {
   return (
     <div className="-mb-4 md:-mb-6">
       <div className="text-center">
-        <p className="text-god-brown py-1">갓생 지수 테스트</p>
+        {/* <span className="p-2 font-minhye text-2xl font-bold">갓생 지수 테스트</span>
+        <p className="text-5xl p-2 font-middleschool_student">나의 갓생 유형은?</p> */}
+        <p className="text-god-brown py-1 ">갓생 지수 테스트</p>
         <p className="text-gray-700 text-3xl font-bold py-1">나의 갓생 유형은?</p>
       </div>
       <div className="text-center py-8">
         <img src="./images/god2.png" alt="가보자고" />
       </div>
       <div className="text-center bg-god-mint rounded-xl">
-        <Link to="/step?page=1" className="text-white font-bold px-6 py-2 inline-flex items-center justify-center">
+        <Link
+          to="/step?page=1"
+          className="text-white font-bold px-6 py-2 inline-flex items-center justify-center">
           <IoIosArrowForward size="16" className="inline-block" />
           <span className="inline-block px-2">갓생 유형 START</span>
         </Link>
